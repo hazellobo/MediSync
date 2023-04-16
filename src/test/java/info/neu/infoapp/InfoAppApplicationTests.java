8d17282c06ab6f2d55dc0cf1abecd484feea585d
Hazel Lobo <lobo.ha@northeastern.edu>
2023-04-15 23:20:34 -0400

    Updated payload validation file

---
+++ src/test/java/info/neu/infoapp/InfoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package info.neu.infoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
